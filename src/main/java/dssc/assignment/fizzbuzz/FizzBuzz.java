c6595bded10c59838ea19023d7dc577e1a6eba78
Irene Brugnara <irene.brugnara@leader.it>
2020-11-18 17:06:00 +0100

    Implemented basic version of FizzBuzz

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,30 @@
 package dssc.assignment.fizzbuzz;
 
+
 public class FizzBuzz {
 
     public static void main(String... args) {
-        
+
+        boolean a3=false, a5=false;
+        for(int i=1; i<=100; i++) {
+            if (i % 3 == 0) {
+                a3 = true;
+                System.out.print("Fizz");
+            }
+            if (i % 5 == 0) {
+                a5 = true;
+                System.out.print("Buzz");
+            }
+            if(a3 || a5) {
+                System.out.println();
+                a3 = a5 = false;
+            }
+            else
+                System.out.println(i);
+        }
+
+
+
     }
 
 }
